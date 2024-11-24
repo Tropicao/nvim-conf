1b3d05117d797b0e33a87301aa149c76b5121a6c
Alexis Lothoré <alexis.lothore@bootlin.com>
2024-11-24 12:13:12 +0100

    lsp: fix autocompletion
    - add luasnip
    - make sure that lsp server is set up with snippets
    - add shortcuts to navigate snippets inserted with autocompletion:
      - Tab to get to the next pre-filled argument
      - Shift-Tab to get to the previous one
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,8 +1,13 @@
 require('mason').setup({})
+require('mason-lspconfig').setup()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 require('mason-lspconfig').setup({
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+	ensure_installed = { "clangd" },
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({ capabilities = capabilities })
+		end,
+	},
 })
+
--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -3,6 +3,7 @@ local cmp = require('cmp')
 cmp.setup({
 	sources = {
 		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
 	},
 	mapping = cmp.mapping.preset.insert({
 		-- Navigate between completion items
@@ -15,12 +16,39 @@ cmp.setup({
 		-- Scroll up and down in the completion documentation
 		['<C-u>'] = cmp.mapping.scroll_docs(-4),
 		['<C-d>'] = cmp.mapping.scroll_docs(4),
-		['<CR>'] = cmp.mapping.confirm({select = false}),
+		['<CR>'] = cmp.mapping.confirm({select = true}),
+		['<Tab>'] = cmp.mapping(function(fallback)
+			local luasnip = require('luasnip')
+			local col = vim.fn.col('.') - 1
+
+			if cmp.visible() then
+				cmp.select_next_item({ behavior = 'select' })
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { 'i', 's' }),
+
+		-- Super shift tab
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			local luasnip = require('luasnip')
+
+			if cmp.visible() then
+				cmp.select_prev_item({ behavior = 'select' })
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
 
 	}),
 	snippet = {
 		expand = function(args)
-			vim.snippet.expand(args.body)
+			require('luasnip').lsp_expand(args.body)
 		end
 	},
 })
